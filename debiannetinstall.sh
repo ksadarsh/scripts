d475cf68cbaf8af18738c49796a323faf1592007
Adarsh Bhat <kanakbhat6@gmail.com>
2020-05-19 19:10:46 +0530

    Comment : S1.5 on Tue May 19 19:10:46 IST 2020

--- debiannetinstall.sh
+++ debiannetinstall.sh
@@ -7,7 +7,7 @@ sudo apt upgrade
 sudo apt install xorg
 sudo apt install lightdm --no-install-recommends
 sudo apt install pcmanfm --no-install-recommends
-sudo apt install ibfm-modules lxinput lxrandr arandr mlocate mousepad sakura gtk2-engines
+sudo apt install libfm-modules lxinput lxrandr arandr mlocate mousepad sakura gtk2-engines
 Sudo apt install gtk2-engines-murrine --no-install-recommends
 sudo apt install ntp ntpdate ufw cbatticon software-properties-common gnome-font-viewer gnome-disk-utility numlockx wmctrl gdebi synaptic git curl wget rofi dmenu lxappearance geany cmake dunst build-essential htop file-roller compton lxpolkit dkms bleachbit gvfs gvfs-backends volumeicon-alsa alsa-utils pavucontrol yad acpid upower tlp nitrogen feh locales-all cmst libgtk3-perl dmz-cursor-theme ttf-dejavu psmisc acpid
 git clone https://github.com/ksadarsh/JWM.git
