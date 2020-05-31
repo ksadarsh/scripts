5caa9eed273a98d39c8d6522f0ff4c949bbdc466
ksadarsh <59079003+ksadarsh@users.noreply.github.com>
2020-05-31 10:53:30 +0530

    Add files via upload

---
+++ run_first.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+sudo apt install firmware-linux xorg
+
+sudo apt install lightdm --no-install-recommends -yy
+
+sudo apt install bspwm sxhkd xdo lemonbar -yy
+
+sudo apt install pcmanfm gtk2-engines-murrine --no-install-recommends -yy
+
+sudo apt install curl wget yad synaptic gdebi numlockx
+
+sudo apt install gvfs gvfs-backends libfm-modules eject gnome-themes-standard mlocate sakura geany geany-plugins -yy
+
+sudo apt install gtk2-engines dmz-cursor-theme lxappearance lxinput lxrandr ntp ntpdate zip unzip tar xarchiver -yy
+
+sudo apt install gparted gufw bleachbit nitrogen feh cbatticon rofi dunst dmenu volumeicon-alsa compton conky alsa-utils gnome-font-viewer gnome-disk-utility -yy
+
+sudo apt install pavucontrol ttf-dejavu fonts-ubuntu xbacklight scrot acpid locales-all autoconf cmake wmctrl libgtk3-perl -yy
+
+sudo apt install python-pip python3-pip handbrake audacity ffmpegthumbnailer simplescreenrecorder parole uget transmission-gtk gimp evince -yy
+
+sudo apt install psmisc cmake xserver-xorg-input-synaptics papirus-icon-theme wicd -yy
+
+sudo apt install atmel-firmware b43-fwcutter bluez-firmware dahdi-firmware-nonfree firmware-ath9k-htc firmware-atheros firmware-b43-installer firmware-b43legacy-installer firmware-bnx2 firmware-bnx2x firmware-brcm80211 firmware-cavium firmware-intelwimax firmware-iwlwifi firmware-libertas firmware-myricom firmware-netronome firmware-netxen firmware-qlogic firmware-ralink firmware-realtek firmware-ti-connectivity firmware-zd1211 iw openssh-client pcmciautils rfkill -yy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+_____________________________________________________
+
+Wireless firmware...
+_____________________________________________________
+
+
+
+
+
+
+
+
+
